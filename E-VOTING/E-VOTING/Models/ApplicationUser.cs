eacf6170ca2031b7cf7e39fc187db8699516666a
Leon Shala <ls48316@ubt-uni.net>
2021-05-28 12:11:32 +0200

    Register

---
+++ E-VOTING/E-VOTING/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_VOTING.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string username { get; set; }
+    }
+}
