9d1f4291a475c90ddb1764b5d3977442c4df0f9e
Leon Shala <ls48316@ubt-uni.net>
2021-05-28 13:12:18 +0200

    update register

--- E-VOTING/E-VOTING/Models/ApplicationUser.cs
+++ E-VOTING/E-VOTING/Models/ApplicationUser.cs
@@ -9,5 +9,9 @@ namespace E_VOTING.Models
     public class ApplicationUser : IdentityUser
     {
         public string username { get; set; }
+        public int NrLeter { get; set; }
+        public string email { get; set; }
+        public string Password { get; set; }
+
     }
 }
---
+++ E-VOTING/E-VOTING/Models/ApplicationUserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_VOTING.Models
+{
+    public class ApplicationUserModel
+    {
+        public string userName { get; set; }
+        public int nrLeternjoftimit { get; set; }
+        public string email { get; set; }
+       
+        public string Password { get; internal set; }
+    }
+}
