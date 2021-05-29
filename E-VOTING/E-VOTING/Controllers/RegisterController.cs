614d1c955ec31ac18af7d06d437bbb50570444ad
Leon Shala <ls48316@ubt-uni.net>
2021-05-29 18:24:29 +0200

    register controller

---
+++ E-VOTING/E-VOTING/Controllers/RegisterController.cs
@@ -0,0 +1,86 @@
+﻿using E_VOTING.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_VOTING.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _env;
+
+        public RegisterController(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            _configuration = configuration;
+            _env = env;
+        }
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                    select votuesi_id, nrLeternjoftimit, email,Password               
+                    from dbo.Login
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("IdentityConnection");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        public JsonResult Post(Register reg)
+        {
+            string query = @"
+                    insert into dbo.Login 
+                    (username,nrLeternjoftimit,email,Password)
+                    values 
+                    (
+                    '" + reg.username + @"'
+                    ,'" + reg.nrLeternjoftimit + @"'
+                    ,'" + reg.email + @"'
+                    ,'" + reg.Password + @"'
+                    )
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("IdentityConnection");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+        }
+    }
+}
--- E-VOTING/E-VOTING/Models/Register.cs
+++ E-VOTING/E-VOTING/Models/Register.cs
@@ -9,14 +9,15 @@ namespace E_VOTING.Models
 {
     public class Register
     {
-        public string name { get; set; }
-        [Required ]
+        public int votuesi_id { get; set; }
+        public string username { get; set; }
+        [Required]
         public int nrLeternjoftimit { get; set; }
         [Required]
         public string email { get; set; }
         [Required]
-        public string password { get; set; }
-        [Required]
-
+        public string Password { get; set; }
+        
+    
     }
 }
