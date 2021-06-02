2aa8ee8eaa3171632ddec8b9f33a6f85979a438a
Rilind Selmani <rilindselmani01@gmail.com>
2021-06-02 23:38:55 +0200

    Deputetet

--- E-VOTING/E-VOTING/Controllers/DeputetetController.cs
+++ E-VOTING/E-VOTING/Controllers/DeputetetController.cs
@@ -1,16 +1,165 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using E_VOTING.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace E_VOTING.Controllers
 {
-    public class DeputetetController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeputetetController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _env;
+
+
+        public DeputetetController(IConfiguration configuration, IWebHostEnvironment env)
         {
-            return View();
+            _configuration = configuration;
+            _env = env;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+              select deputetet_id, Partia, Emri
+                 from dbo.Deputetet
+                 ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("IdentityConnection");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+
+            }
+            return new JsonResult(table);
+
+        }
+        [HttpPost]
+        public JsonResult Post(Deputetet dep)
+        {
+            string query = @"
+              insert into dbo.Deputetet
+               (Partia, Emri)
+                values  
+                    (
+                    '" + dep.Partia + @"'
+                    ,'" + dep.Emri + @"'
+                    )
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("IdentityConnection");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+
+            }
+            return new JsonResult("Added Succesfully");
+        }
+        [HttpPut]
+        public JsonResult Put(Deputetet dep)
+        {
+            string query = @"
+               update dbo.Deputetet set
+               Partia = '" + dep.Partia + @"'
+               ,Emri = '" + dep.Emri + @"'
+                where deputetet_id = " + dep.deputetet_id + @"
+                 ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("IdentityConnection");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+
+            }
+            return new JsonResult("Updated Succesfully");
+        }
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                    delete from dbo.Deputetet
+                    where deputetet_id = " + id + @" 
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("IdentityConnection");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Successfully");
+        }
+
+
+        [Route("GetAllPartit")]
+        public JsonResult GetAllPartit()
+        {
+            string query = @"
+                    select Deputetet from dbo.Deputetet
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("IdentityConnection");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
         }
     }
 }
--- E-VOTING/E-VOTING/Models/Deputetet.cs
+++ E-VOTING/E-VOTING/Models/Deputetet.cs
@@ -7,5 +7,11 @@ namespace E_VOTING.Models
 {
     public class Deputetet
     {
+        public int deputetet_id { get; set; }
+
+        public string Partia { get; set; }
+
+        public string Emri { get; set; }
+
     }
 }
