fb9eb0f449f071465dd37c79be5eb4eafeddbcb8
gramos xhigoli <gramosxh@gmail.com>
2021-06-02 12:00:58 +0200

    step1

---
+++ E-VOTING/E-VOTING/Models/Shtetet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_VOTING.Models
+{
+    public class Shtetet
+    {
+
+        public int IDShteti { get; set; }
+
+        public string Shteti { get; set; }
+
+}
+}
