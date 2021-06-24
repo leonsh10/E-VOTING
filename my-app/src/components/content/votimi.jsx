16ad03a97b44c151bfdbababcab8b0bd65e9bd6d
florian alija <fa47799@ubt-uni.net>
2021-06-24 22:01:18 +0200

    Home page progress

--- my-app/src/components/content/votimi.jsx
+++ my-app/src/components/content/votimi.jsx
@@ -1,12 +1,18 @@
 import React,{Component} from 'react';
+import '../../App.scss';
 
 export class votimi extends Component{
 
 
 render(){
     return(
-    <h1>Forma e Votimit</h1>
-
+    <div class="componentsHome">
+        <div class="homeContent">
+            <h1>PER CFARE ARSYJE PERDORET E-VOTING</h1>
+            <p>E-Voting perdoret per arsyje se eshte menyra me e mire dhe me e sigurte per votim ne tere boten. Qe nga viti 2021 kjo menyre e votimit perdoret edhe ne Kosove</p>
+            <button class="home-btn">SI TE VOTONI</button>
+        </div>
+    </div>
     )
 }
 
