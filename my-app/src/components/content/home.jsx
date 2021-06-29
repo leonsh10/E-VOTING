dfbb511b6c324d7c706b01ee85b61f39020e1e1c
Leon Shala <ls48316@ubt-uni.net>
2021-06-29 21:42:19 +0200

    videoHomePage

--- my-app/src/components/content/home.jsx
+++ my-app/src/components/content/home.jsx
@@ -1,30 +1,50 @@
 import React,{Component} from 'react';
 import '../../App.scss';
 import video1 from "../videos/video1.mp4";
+import {Button} from 'react-bootstrap';
+import ScrollButton from 'react-scroll-button'
 
-export class home extends Component{
-    render(){
+
+// export class home extends Component{
+function home() {
+    function handleScroll() {
+        window.scroll({
+          top: document.body.offsetHeight,
+          left: 0, 
+          behavior: 'smooth',
+        });
+      }
+          
+    // render();{
         return(
             <div class="componentsHome">
                 <div class="homeContent">
                     <h1>PER CFARE ARSYJE PERDORET E-VOTING</h1>
                     <p>E-Voting perdoret per arsyje se eshte menyra me e mire dhe me e sigurte per votim ne tere boten. Qe nga viti 2021 kjo menyre e votimit perdoret edhe ne Kosove</p>
-                    <button class="home-btn">SI TE VOTONI</button>
+                    <button class="home-btn" onClick={handleScroll}>SI TE VOTONI</button>
+
+                    {/* <ScrollButton 
+                behavior={'smooth'} 
+                buttonBackgroundColor={'red'}
+                iconType={'arrow-up'}
+                style= {{fontSize: '30px'}}
+            /> */}
                 </div>
                 <div class="videoDiv">
                     <div class="rainbow">
                 <video src={video1} width="600" height="300" class="vid" controls="controls" autoplay="false" />
                 </div>
                 <div class="pershkrimi">
-                <h1>Pershkrimi i videos</h1>
-                <p>E-Voting perdoret per arsyje se eshte menyra me e mire dhe me e sigurte per votim ne tere boten. Qe nga viti 2021 kjo menyre e votimit perdoret edhe ne Kosove</p>
+                <h1>Si te votoni?</h1>
+                <p>Qe te kryeni votimin me sukses ne kemi pergatitur nje video tutorial ku tregojme hap pas hapi se si duhet te votoni.Per qdo paqartesi klikoni butonin me poshte qe te kontaktoni me ne.</p>
+                <button class="home-btn" >Contact Us</button>
                 </div>
                 </div>
 
             
             </div>
         )
-    }
-}
-
+    // }
+// }
+        }
 export default home
