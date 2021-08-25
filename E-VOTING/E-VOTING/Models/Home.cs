4fb647fc2be24e148b93f189fc1058947d343f64
florian alija <fa47799@ubt-uni.net>
2021-08-25 16:46:03 +0200

    backend edit

--- E-VOTING/E-VOTING/Models/Home.cs
+++ E-VOTING/E-VOTING/Models/Home.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace E_VOTING.Models
 {
     public class Home
     {
+        [Key]
         public int idHome { get; set; }
 
         public string Titulli { get; set; }
