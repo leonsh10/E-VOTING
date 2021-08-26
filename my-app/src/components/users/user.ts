bd761e0aa6bb28a4a10a1003910237723419101b
Leon Shala <ls48316@ubt-uni.net>
2021-08-26 13:34:05 +0200

    LoginForm

---
+++ my-app/src/components/users/user.ts
@@ -0,0 +1,13 @@
+export interface User {
+    username: string;
+    email: string;
+    nrLeternjoftimit: string;
+    token:string;
+}
+
+export interface UserFormValues{
+    nrLeternjoftimit: string;
+    email: string;
+    password: string;
+    
+}
